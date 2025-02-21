c5a6f7c94febd5c256deb345bab89b9b91d66340
sc-alexp <152201418+sc-alexp@users.noreply.github.com>
2025-02-21 12:43:19 -0600

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,7 +15,7 @@ let dvd = {
 (function main() {
   canvas = document.getElementById("tv-screen");
   ctx = canvas.getContext("2d");
-  dvd.img.src = "SCDVD.png";
+  dvd.img.src = "allen.png";
 
   // Draw the "tv screen"
   canvas.width = window.innerWidth;
